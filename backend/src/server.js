50d4ba1937e2b1066a3dd284178f29031521f7f7
TeeGreat <toyinatofarati64@gmail.com>
2025-08-10 14:54:47 +0100

    Server setup

---
+++ backend/src/server.js
@@ -0,0 +1,92 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
+
+const app = express();
+const PORT = ENV.PORT || 8001;
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+  });
+});
+
+// Add fav
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavorite[0]);
+  } catch (error) {
+    console.log("Error adding favorite", error);
+    res.status(500).json("Something went wrong:", error);
+  }
+});
+
+// delete fav
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(
+        and(
+          eq(favoritesTable.userId, userId),
+          eq(favoritesTable.recipeId, parseInt(recipeId))
+        )
+      );
+
+    res.status(200).json({
+      message: "Favorite removed successfully",
+    });
+  } catch (error) {
+    console.log("Error removing favorite", error);
+    res.status(500).json("Something went wrong:", error);
+  }
+});
+
+// Get fav
+app.get("/api/favorites/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavourite = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+    res.status(200).json(userFavourite);
+  } catch (error) {
+    console.log("Error fetching favorite", error);
+    res.status(500).json("Something went wrong:", error);
+  }
+});
+
+app.listen(5001, () => {
+  console.log("Server running on PORT:", PORT);
+});
